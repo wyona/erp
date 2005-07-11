57fe914e42ca546cf93606eb8d44ac6a42c1ebf2
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-07-11 07:24:19 +0000

    project and invoice doctye added

---
+++ src/java/org/wyona/erp/doctypes/Invoice.java
@@ -0,0 +1,15 @@
+package org.wyona.erp.doctypes;
+
+/**
+ *
+ */
+public class Invoice {
+    String customer;
+
+    /**
+     *
+     */
+    public Invoice(String customer) {
+        this.customer = customer;
+    }
+}
---
+++ src/java/org/wyona/erp/doctypes/Project.java
@@ -0,0 +1,17 @@
+package org.wyona.erp.doctypes;
+
+/**
+ *
+ */
+public class Project {
+    String title;
+    String customer;
+    String description;
+
+    /**
+     *
+     */
+    public Project(String title) {
+        this.title = title;
+    }
+}
