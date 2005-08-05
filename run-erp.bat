5b54bd14df921abe2bc1b89b82f468d5cbf5aabd
oli <oli@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-08-05 10:07:10 +0000

    better readable %CP%

--- run-erp.bat
+++ run-erp.bat
@@ -11,4 +11,15 @@
 set MAVEN_REPO=C:\tmp\.maven\repository
 
 :: set CLASSPATH
-set CP=D:\lenyatest_1.4\erp\trunk\build\classes;%MAVEN_REPO%\jsr170\jars\jcr-1.0.jar;%MAVEN_REPO%\log4j\jars\log4j-1.2.9.jar;%MAVEN_REPO%\jackrabbit\jars\jackrabbit-api-1.0-dev-LCR209797.jar;%MAVEN_REPO%\xerces\jars\xerces-2.4.0.jar;%MAVEN_REPO%\jackrabbit\jars\jackrabbit-commons-1.0-dev-LCR209797.jar;%MAVEN_REPO%\jackrabbit\jars\jackrabbit-core-1.0-dev-LCR209797.jar;%MAVEN_REPO%\commons-collections\jars\commons-collections-3.1.jar;%MAVEN_REPO%\xml-apis\jars\xml-apis-1.0.b2.jar;%MAVEN_REPO%\concurrent\jars\concurrent-1.3.4.jar;%MAVEN_REPO%\lucene\jars\lucene-1.4.3.jar;D:\lenyatest_1.4\erp\trunk\lib
+set CP=D:\lenyatest_1.4\erp\trunk\build\classes
+set CP=%CP%;%MAVEN_REPO%\jsr170\jars\jcr-1.0.jar
+set CP=%CP%;%MAVEN_REPO%\log4j\jars\log4j-1.2.9.jar
+set CP=%CP%;%MAVEN_REPO%\jackrabbit\jars\jackrabbit-api-1.0-dev-LCR209797.jar
+set CP=%CP%;%MAVEN_REPO%\xerces\jars\xerces-2.4.0.jar
+set CP=%CP%;%MAVEN_REPO%\jackrabbit\jars\jackrabbit-commons-1.0-dev-LCR209797.jar
+set CP=%CP%;%MAVEN_REPO%\jackrabbit\jars\jackrabbit-core-1.0-dev-LCR209797.jar
+set CP=%CP%;%MAVEN_REPO%\commons-collections\jars\commons-collections-3.1.jar
+set CP=%CP%;%MAVEN_REPO%\xml-apis\jars\xml-apis-1.0.b2.jar
+set CP=%CP%;%MAVEN_REPO%\concurrent\jars\concurrent-1.3.4.jar
+set CP=%CP%;%MAVEN_REPO%\lucene\jars\lucene-1.4.3.jar
+set CP=%CP%;D:\lenyatest_1.4\erp\trunk\lib
