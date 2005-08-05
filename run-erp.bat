4c33f32ca59802d85cfcbaa0808e5a3c9d6b848b
oli <oli@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-08-05 14:01:59 +0000

    java command, variables, still needs a good eye on windows that paths are correct.

--- run-erp.bat
+++ run-erp.bat
@@ -1,17 +1,25 @@
 :: Intializing script for ERP / WIN32
 :: version 1.a
 
-:: !! This is just a temporary Batchfile for the Classpath.
+:: 
 
 :: 05.08.05, Oliver Schalch
 
 @echo off
 
-:: set maven repository
-set MAVEN_REPO=C:\tmp\.maven\repository
+:: JAVA_BIN, JAVA_HOME
 
-:: set CLASSPATH
-set CP=D:\lenyatest_1.4\erp\trunk\build\classes
+set JAVA_BIN=C:\j2sdk1.4.2_08\bin
+set JAVA_HOME=C:\j2sdk1.4.2_08
+
+:: set maven repository
+set MAVEN_REPO=D:\lenyatest_1.4\src\.maven\repository
+
+:: set CLASSPATH (remember that CLASSPATH dont like spaces)
+
+:: need changes to your system
+set CP=D:\lenyatest_1.4\src\erp-trunk\build\classes
+
 set CP=%CP%;%MAVEN_REPO%\jsr170\jars\jcr-1.0.jar
 set CP=%CP%;%MAVEN_REPO%\log4j\jars\log4j-1.2.9.jar
 set CP=%CP%;%MAVEN_REPO%\jackrabbit\jars\jackrabbit-api-1.0-dev-LCR209797.jar
@@ -22,4 +30,9 @@ set CP=%CP%;%MAVEN_REPO%\commons-collections\jars\commons-collections-3.1.jar
 set CP=%CP%;%MAVEN_REPO%\xml-apis\jars\xml-apis-1.0.b2.jar
 set CP=%CP%;%MAVEN_REPO%\concurrent\jars\concurrent-1.3.4.jar
 set CP=%CP%;%MAVEN_REPO%\lucene\jars\lucene-1.4.3.jar
-set CP=%CP%;D:\lenyatest_1.4\erp\trunk\lib
+
+:: need changes to your system
+set CP=%CP%;D:\lenyatest_1.4\src\erp-trunk\lib
+
+
+java -cp %CP% -Djava.security.auth.login.config==jaas.config org.wyona.erp.cli.CLI %1 %2 %3 %4 %5 %6 %7 %8 %9
