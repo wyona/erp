9d8f91159de505eed4c18a1980bf2f506d8f5173
edith <edith@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-09-21 16:06:41 +0000

    Make the erp compatible with jeceira

--- run-erp.sh
+++ run-erp.sh
@@ -5,12 +5,8 @@ MAVEN_REPO=~/.maven/repository
 CLASSPATH=build/lib/erp-cli.jar
 #CLASSPATH=build/classes
 CLASSPATH=$CLASSPATH:$MAVEN_REPO/jsr170/jars/jcr-1.0.jar
-#CLASSPATH=$CLASSPATH:lib/jcr-0.16.4.1.jar
 CLASSPATH=$CLASSPATH:$MAVEN_REPO/log4j/jars/log4j-1.2.9.jar
-CLASSPATH=$CLASSPATH:$MAVEN_REPO/jackrabbit/jars/jackrabbit-api-1.0-dev-LCR209797.jar
-CLASSPATH=$CLASSPATH:$MAVEN_REPO/jackrabbit/jars/jackrabbit-commons-1.0-dev-LCR209797.jar
-CLASSPATH=$CLASSPATH:$MAVEN_REPO/jackrabbit/jars/jackrabbit-core-1.0-dev-LCR209797.jar
-#CLASSPATH=$CLASSPATH:lib/jackrabbit-0.16.4.1-dev.jar
+
 CLASSPATH=$CLASSPATH:$MAVEN_REPO/springframework/jars/spring-1.2.4.jar
 CLASSPATH=$CLASSPATH:$MAVEN_REPO/commons-logging/jars/commons-logging-1.0.4.jar
 CLASSPATH=$CLASSPATH:$MAVEN_REPO/commons-collections/jars/commons-collections-3.1.jar
@@ -21,4 +17,26 @@ CLASSPATH=$CLASSPATH:$MAVEN_REPO/lucene/jars/lucene-1.4.3.jar
 CLASSPATH=$CLASSPATH:src/spring
 CLASSPATH=$CLASSPATH:lib
 
-java -cp $CLASSPATH -Djava.security.auth.login.config==jaas.config org.wyona.erp.cli.CLI $*
+#
+#for Jackrabbit
+#
+#CLASSPATH=$CLASSPATH:$MAVEN_REPO/jackrabbit/jars/jackrabbit-api-1.0-dev-LCR209797.jar
+#CLASSPATH=$CLASSPATH:$MAVEN_REPO/jackrabbit/jars/jackrabbit-commons-1.0-dev-LCR209797.jar
+#CLASSPATH=$CLASSPATH:$MAVEN_REPO/jackrabbit/jars/jackrabbit-core-1.0-dev-LCR209797.jar
+#
+#java -cp $CLASSPATH -Djava.security.auth.login.config==jaas.config org.wyona.erp.cli.CLI $*
+
+#
+#for jeceira
+#
+CLASSPATH=$CLASSPATH:src/jeceira
+CLASSPATH=$CLASSPATH:$MAVEN_REPO/jeceira/jars/jeceira-0.1.1-maintenance20050909T0933.jar
+CLASSPATH=$CLASSPATH:lib/doka-0.1.jar
+CLASSPATH=$CLASSPATH:lib/commons-0.1.jar
+CLASSPATH=$CLASSPATH:$MAVEN_REPO/commons-lang/jars/commons-lang-2.0.jar
+CLASSPATH=$CLASSPATH:lib/hsqldb.jar
+#CLASSPATH=$CLASSPATH:$MAVEN_REPO/hsqldb/jars/hsqldb-1.8.0.1.jar 
+CLASSPATH=$CLASSPATH:lib/jug-asl-2.0rc4.jar
+CLASSPATH=$CLASSPATH:lib/jug-native/
+
+java -cp $CLASSPATH org.wyona.erp.cli.CLI $*
