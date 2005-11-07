60fb97e73958189a66620536f9973086f1420dce
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-11-07 23:52:43 +0000

    property handling started

--- may/src/main/java/org/wyona/may/App.java
+++ may/src/main/java/org/wyona/may/App.java
@@ -12,5 +12,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         DataType dt = new DataType(new File("data-types/odt/jcr.xml"));
+        String[] properties = dt.getProperties();
+        for (int i = 0; i < properties.length; i++) {
+            System.out.println(dt.getPropertyType(properties[i]));
+        }
     }
 }
--- may/src/main/java/org/wyona/may/DataType.java
+++ may/src/main/java/org/wyona/may/DataType.java
@@ -13,4 +13,36 @@ public class DataType {
     public DataType(File config) {
         System.out.println(config);
     }
+
+    /**
+     *
+     */
+    public String[] getProperties() {
+        return new String[0];
+    }
+
+    /**
+     *
+     */
+    public String getPropertyType(String property) {
+        return null;
+    }
+
+    /**
+     *
+     */
+    public void setProperty(String property, Object object) {
+    }
+
+    /**
+     * This should rather be handled by a Writer!
+     */
+    public void write(String path) {
+    }
+
+    /**
+     * This should rather be handled by a Reader!
+     */
+    public void read(String path) {
+    }
 }
