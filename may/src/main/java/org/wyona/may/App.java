19c46af4de93186abeb581664eea212fd333678f
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-11-10 16:36:13 +0000

    pseudo code added

--- may/src/main/java/org/wyona/may/App.java
+++ may/src/main/java/org/wyona/may/App.java
@@ -7,7 +7,7 @@ import java.io.File;
  */
 public class App {
     /**
-     *
+     * Also see Hibernate or OJB, Graffito or Jackrabbit node-type definition (lenya_1_4_x/src/modules/jackrabbit/repository/repository/nodetypes)
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -16,5 +16,23 @@ public class App {
         for (int i = 0; i < properties.length; i++) {
             System.out.println(dt.getPropertyType(properties[i]));
         }
+
+/*
+        Repository repo = RepositoryFactory.getRepository("wyona");
+
+        Invoice invoice = new Invoice(DataTypeRegistry.getDataType("invoice","http://www.wyona.com/erp/1.0"));
+        invoice.setCustomer("...");
+
+        Email email = new Email(DataTypeRegistry.getDataType("email","http://www.wyona.com/erp/1.0"));
+        email.setFrom("michael.wechner@wyona.com");
+        email.setSubject("Hello World");
+        email.setSource("...");
+
+        DataTypeWriter writer = repo.getWriter("/invoices/foobar");
+        writer.write(invoice);
+
+        DataTypeReader reader = repo.getReader("/invoices/foobar");
+        Invoice inv = reader.read();
+*/
     }
 }
