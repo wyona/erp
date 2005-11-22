1532c7c31dfb99ae5503d42e339d409d4e6b1a31
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-11-22 14:37:52 +0000

    stop script added

---
+++ stop.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "WARNING: This script is still under development and not ready for productive usage"
+
+echo "Shutdown Apache httpd as Load Balancer"
+./build/httpd-2.1.9-beta/bin/apachectl stop
+
+echo "Shutdown Tomcat as Cluster"
+./build/cnodes/jakarta-tomcat-5.0.30-cnode1/bin/shutdown.sh
