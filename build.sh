8ff47e496a4782f06a50dcd3dcf1a95b635234ab
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-11-22 21:44:21 +0000

    sed added

--- build.sh
+++ build.sh
@@ -2,6 +2,25 @@
 
 echo "This script is under development and not meant to be used for productive usage"
 
-diff tools/loadbalancing-cluster/lbs/httpd.conf build/httpd-2.1.9-beta/conf/httpd.conf
+
+echo "Create build directory"
+mkdir build
+
+
+echo "Build Apache httpd"
+cd build
+BUILD_DIR=`pwd`
+#tar -xzf ../tools/loadbalancing-cluster/lbs/httpd-2.1.9-beta.tar.gz
+#mv httpd-2.1.9-beta httpd-2.1.9-beta-src
+#cd httpd-2.1.9-beta-src
+#sh configure --prefix=$BUILD_DIR/httpd-2.1.9-beta
+#make
+#make install
+#cd ..
+#rm -r httpd-2.1.9-beta-src
+cd ..
+##diff tools/loadbalancing-cluster/lbs/httpd.conf build/httpd-2.1.9-beta/conf/httpd.conf
+sed -e 's\@PREFIX@\/home/michi/src/erp/trunk/build/httpd-2.1.9-beta\g' tools/loadbalancing-cluster/lbs/httpd.conf > build/httpd-2.1.9-beta/conf/httpd.conf
+##sed -e 's\@PREFIX@\$BUILD_DIR/httpd-2.1.9-beta\g' tools/loadbalancing-cluster/lbs/httpd.conf > build/httpd-2.1.9-beta/conf/httpd.conf
 cp tools/loadbalancing-cluster/lbs/httpd-vhosts.conf build/httpd-2.1.9-beta/conf/extra/httpd-vhosts.conf
 cp tools/loadbalancing-cluster/lbs/workers.properties build/httpd-2.1.9-beta/conf/.
