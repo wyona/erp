f6e305e7bcbb280e170642d24caef3c194528d99
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-11-26 21:57:52 +0000

    instantiate DataType from reader and input stream

--- may/src/main/java/org/wyona/may/core/DataType.java
+++ may/src/main/java/org/wyona/may/core/DataType.java
@@ -1,7 +1,11 @@
 package org.wyona.may.core;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.tools.ant.util.ReaderInputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -16,14 +20,48 @@ import org.xml.sax.helpers.DefaultHandler;
 public class DataType {
 
     /**
-     *
+     * @param file The XML file to be read
      */
-    public DataType(File xmlFile) throws IOException {
-        System.out.println(xmlFile);
+    public DataType(File file) throws IOException {
+        System.out.println(file);
         try {
             SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
             //DefaultHandler saxHandler = new DataTypeHandler();
-            saxParser.parse(xmlFile, new DefaultHandler());
+            saxParser.parse(file, new DefaultHandler());
+        } catch (ParserConfigurationException e) {
+            System.err.println(e);
+            throw new IOException(e.toString());
+        } catch (SAXException e) {
+            System.err.println(e);
+            throw new IOException(e.toString());
+        }
+    }
+
+    /**
+     *
+     */
+    public DataType(InputStream is) throws IOException {
+        try {
+            SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+            //DefaultHandler saxHandler = new DataTypeHandler();
+            saxParser.parse(is, new DefaultHandler());
+        } catch (ParserConfigurationException e) {
+            System.err.println(e);
+            throw new IOException(e.toString());
+        } catch (SAXException e) {
+            System.err.println(e);
+            throw new IOException(e.toString());
+        }
+    }
+
+    /**
+     *
+     */
+    public DataType(Reader reader) throws IOException {
+        try {
+            SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+            //DefaultHandler saxHandler = new DataTypeHandler();
+            saxParser.parse(new ReaderInputStream(reader), new DefaultHandler());
         } catch (ParserConfigurationException e) {
             System.err.println(e);
             throw new IOException(e.toString());
