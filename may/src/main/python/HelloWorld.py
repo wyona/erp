c80e0f1aa1b7c5bf4d17d018c01e86a2e910785b
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2005-12-23 23:45:15 +0000

    python example started

---
+++ may/src/main/python/HelloWorld.py
@@ -0,0 +1,10 @@
+# http://www.ferg.org/projects/python_java_side-by-side.html
+# http://www.xml.com/pub/a/1999/12/xml99/python.html
+
+class HelloWorld:
+
+  def __init__(self):
+    self.name = "Michi"
+
+  def sayHi(name):
+    print "Hello ", name, self.name
