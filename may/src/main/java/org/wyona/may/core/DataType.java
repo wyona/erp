45505c88a9b08e5a916559c39122b0bd810f5fb4
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-01-01 22:54:34 +0000

    DataTypeHandler used

--- may/src/main/java/org/wyona/may/core/DataType.java
+++ may/src/main/java/org/wyona/may/core/DataType.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.io.Reader;
 
+import org.apache.log4j.Category;
 import org.apache.tools.ant.util.ReaderInputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -18,6 +19,9 @@ import org.xml.sax.helpers.DefaultHandler;
  *
  */
 public class DataType {
+    private static Category log = Category.getInstance(DataType.class);
+
+    private String name;
 
     /**
      * @param file The XML file to be read
@@ -26,8 +30,9 @@ public class DataType {
         System.out.println(file);
         try {
             SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-            //DefaultHandler saxHandler = new DataTypeHandler();
-            saxParser.parse(file, new DefaultHandler());
+            DataTypeHandler dth = new DataTypeHandler();
+            saxParser.parse(file, dth);
+            init(dth);
         } catch (ParserConfigurationException e) {
             System.err.println(e);
             throw new IOException(e.toString());
@@ -43,8 +48,9 @@ public class DataType {
     public DataType(InputStream is) throws IOException {
         try {
             SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-            //DefaultHandler saxHandler = new DataTypeHandler();
-            saxParser.parse(is, new DefaultHandler());
+            DataTypeHandler dth = new DataTypeHandler();
+            saxParser.parse(is, dth);
+            init(dth);
         } catch (ParserConfigurationException e) {
             System.err.println(e);
             throw new IOException(e.toString());
@@ -60,8 +66,9 @@ public class DataType {
     public DataType(Reader reader) throws IOException {
         try {
             SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
-            //DefaultHandler saxHandler = new DataTypeHandler();
-            saxParser.parse(new ReaderInputStream(reader), new DefaultHandler());
+            DataTypeHandler dth = new DataTypeHandler();
+            saxParser.parse(new ReaderInputStream(reader), dth);
+            init(dth);
         } catch (ParserConfigurationException e) {
             System.err.println(e);
             throw new IOException(e.toString());
@@ -72,9 +79,17 @@ public class DataType {
     }
 
     /**
+     *
+     */
+    protected void init(DataTypeHandler dth) {
+        name = dth.getDataTypeDefinitionName();
+    }
+
+    /**
      * Get Data Type Definition
      */
     public DataTypeDefinition getDataTypeDefinition() {
+        log.info(name);
         return new DataTypeDefinition(new File("invoice/dt.xml"));
     }
 
